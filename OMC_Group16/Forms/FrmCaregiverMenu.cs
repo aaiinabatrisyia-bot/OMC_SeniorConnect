222f3fd974189d0c97f3db72de2f7dc3c1a8a455
puteri000 <puteri_25006766@utp.edu.my>
2026-06-29 20:09:43 +0800

    Added initial forms

---
+++ OMC_Group16/Forms/FrmCaregiverMenu.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace OMC_Group16
+{
+    public partial class FrmCaregiverMenu : Form
+    {
+        public FrmCaregiverMenu()
+        {
+            InitializeComponent();
+        }
+    }
+}
--- OMC_Group16/Forms/FrmWelcome.cs
+++ OMC_Group16/Forms/FrmWelcome.cs
@@ -1,8 +1,8 @@
 namespace OMC_Group16
 {
-    public partial class Form1 : Form
+    public partial class FrmWelcome : Form
     {
-        public Form1()
+        public FrmWelcome()
         {
             InitializeComponent();
         }
